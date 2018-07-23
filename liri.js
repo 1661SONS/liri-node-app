ca96fce60caf8ee90a2d7b109d2adbe89aa8ad57
1661sons <julia.kerouac@gmail.com>
2018-07-23 00:09:42 -0400

    adding to-do

--- liri.js
+++ liri.js
@@ -1,1 +1,25 @@
 require("dotenv").config();
+
+// commands
+    // my-tweets
+        // prints last 20 tweets and their dates tweeted from @MadamnMarkdown
+    
+    // spotify-this-song
+        // prints song name,
+        // artist(s)
+        // song album
+        // link to song preview
+        // if song can't be found, default to 'what more do you want from me' by al green
+
+    // movie-this
+        // prints movie name 
+        // Year the movie came out.
+        // IMDB Rating of the movie.
+        // Rotten Tomatoes Rating of the movie.
+        // Country where the movie was produced.
+        // Language of the movie.
+        // Plot of the movie.
+        // Actors in the movie.
+        // if no movie, print stats on black panther
+
+    // do-what-it-says
